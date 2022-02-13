9c0936f9a648700ece60596eb268f052582738a5
Nadim El-Jaroudi <nje2494@gmail.com>
2022-02-13 14:02:31 -0500

    Refactored Subgraph To Support Naming Changes
    Adjusted the subgraph to support the new naming conventions used in contracts. Changed Tagging_Record to pull from contract for all info other than tagging id.
    
    [Ticket: 14]

--- packages/subgraph/src/mappings/tagging-record-registry.ts
+++ packages/subgraph/src/mappings/tagging-record-registry.ts
@@ -1,9 +1,9 @@
 // import { BigInt, Bytes, ipfs, json, JSONValue } from "@graphprotocol/graph-ts";
 // import { log } from "@graphprotocol/graph-ts";
 
-import { HashtagRegistered, ERC721HashtagRegistry } from "../generated/ERC721HashtagRegistry/ERC721HashtagRegistry";
-import { HashtagProtocol } from "../generated/HashtagProtocol/HashtagProtocol";
-import { Tag, Hashtag } from "../generated/schema";
+import { TargetTagged,  ETS} from "../generated/ETS/ETS";
+import { ETSTag } from "../generated/ETSTag/ETSTag";
+import { Tag, Tagging_Record } from "../generated/schema";
 
 import {
   toLowerCase,
@@ -18,48 +18,48 @@ import {
 /*
  * Track the tagging of an NFT asset
  *
- * event.params.hashtagId NFT ID of the hashtag
+ * event.params.tagId NFT ID of the tag
  * event.params.nftContract Contract address of the NFT asset being tagged
  * event.params.nftId NFT ID of the NFT asset being tagged
  * event.params.tagger Ethereum address that initiated the tag
  * event.params.publisher Publisher that facilitated the tag
- * event.params.tagFee Fee earned by the Hashtag Protocol
+ * event.params.tagFee Fee earned by the tag Protocol
  * event.params.nftChainId Chain Id the target nft is on
  *
  * Notes
  *    A number of data points are generated off the back of this tagging event:
- *     - Tag / usage count for a hashtag
- *     - Tagging fees earned by the hashtag owner, publisher and platform
+ *     - Tag / usage count for a tag
+ *     - Tagging fees earned by the tag owner, publisher and platform
  */
-export function handleHashtagRegistered(event: HashtagRegistered): void {
-  let registryContract = ERC721HashtagRegistry.bind(event.address);
-  let tagFee = event.params.tagFee;
+export function handleTagRegistered(event: TargetTagged): void {
+  let registryContract = ETS.bind(event.address);
+  let tagFee = registryContract.taggingFee();
 
   let modulo = registryContract.modulo();
   let platformPercentageOfTagFee = registryContract.platformPercentage();
   let publisherPercentageOfTagFee = registryContract.publisherPercentage();
   let remainingPercentage = modulo.minus(platformPercentageOfTagFee).minus(publisherPercentageOfTagFee);
 
-  let protocolAddress = registryContract.hashtagProtocol();
-  let protocolContract = HashtagProtocol.bind(protocolAddress);
+  let protocolAddress = registryContract.etsTag();
+  let protocolContract = ETSTag.bind(protocolAddress);
   let platformAddress = protocolContract.platform();
 
-  let hashtagId = event.params.hashtagId;
-  let owner = protocolContract.ownerOf(hashtagId);
+  let etsTagId = registryContract.getTaggingRecord(event.params.taggingId).value0;
+  let owner = protocolContract.ownerOf(etsTagId);
 
   let publisherFee = tagFee.times(publisherPercentageOfTagFee).div(modulo);
   let platformFee = tagFee.times(platformPercentageOfTagFee).div(modulo);
   let remainingFee = tagFee.times(remainingPercentage).div(modulo);
 
-  let hashtag = Hashtag.load(hashtagId.toString());
+  let etsTag = Tag.load(etsTagId.toString());
 
   // this is a pre-auction state if true or post-auction if false
   if (owner.equals(platformAddress)) {
-    if (hashtag) {
-      hashtag.creatorRevenue = hashtag.creatorRevenue.plus(remainingFee);
+    if (etsTag) {
+      etsTag.creatorRevenue = etsTag.creatorRevenue.plus(remainingFee);
 
       //  Update creator counts and fees
-      let creator = protocolContract.getCreatorAddress(hashtagId);
+      let creator = protocolContract.getCreatorAddress(etsTagId);
       let creatorEntity = safeLoadCreator(creator.toHexString());
 
       if (creatorEntity) {
@@ -69,8 +69,8 @@ export function handleHashtagRegistered(event: HashtagRegistered): void {
       }
     }
   } else {
-    if (hashtag) {
-      hashtag.ownerRevenue = hashtag.ownerRevenue.plus(remainingFee);
+    if (etsTag) {
+      etsTag.ownerRevenue = etsTag.ownerRevenue.plus(remainingFee);
 
       // Update owner counts and fees
       let ownerEntity = safeLoadOwner(owner.toHexString());
@@ -83,30 +83,30 @@ export function handleHashtagRegistered(event: HashtagRegistered): void {
     }
   }
 
-  // Update rest of hashtag data
-  if (hashtag) {
-    hashtag.tagCount = hashtag.tagCount.plus(ONE);
-    hashtag.publisherRevenue = hashtag.publisherRevenue.plus(publisherFee);
-    hashtag.protocolRevenue = hashtag.protocolRevenue.plus(platformFee);
-    hashtag.save();
+  // Update rest of tag data
+  if (etsTag) {
+    etsTag.tagCount = etsTag.tagCount.plus(ONE);
+    etsTag.publisherRevenue = etsTag.publisherRevenue.plus(publisherFee);
+    etsTag.protocolRevenue = etsTag.protocolRevenue.plus(platformFee);
+    etsTag.save();
   }
 
   // Store tag information
-  let tagEntity = new Tag(event.transaction.hash.toHexString());
+  let taggingRecord = new Tagging_Record(event.params.taggingId.toString());
 
-  if (tagEntity && hashtag) {
-    tagEntity.hashtagId = hashtagId.toString();
-    tagEntity.nftContract = event.params.nftContract;
-    tagEntity.nftId = event.params.nftId.toString();
-    tagEntity.nftChainId = event.params.nftChainId;
-    tagEntity.tagger = event.params.tagger;
-    tagEntity.timestamp = event.block.timestamp;
-    tagEntity.publisher = event.params.publisher;
-    tagEntity.hashtagDisplayHashtag = hashtag.displayHashtag;
-    let lowerHashtag = toLowerCase(hashtag.displayHashtag);
-    tagEntity.hashtag = lowerHashtag;
-    tagEntity.hashtagWithoutHash = lowerHashtag.substring(1, lowerHashtag.length);
-    tagEntity.save();
+  if (taggingRecord && etsTag) {
+    taggingRecord.tagId = etsTagId.toString();
+    taggingRecord.nftContract = registryContract.getTaggingRecord(event.params.taggingId).value1;
+    taggingRecord.nftId = registryContract.getTaggingRecord(event.params.taggingId).value2.toString();
+    taggingRecord.nftChainId = registryContract.getTaggingRecord(event.params.taggingId).value6;
+    taggingRecord.tagger = registryContract.getTaggingRecord(event.params.taggingId).value3;
+    taggingRecord.timestamp = registryContract.getTaggingRecord(event.params.taggingId).value4;
+    taggingRecord.publisher = registryContract.getTaggingRecord(event.params.taggingId).value5;
+    taggingRecord.displayTag = etsTag.displayTag;
+    let lowerTag = toLowerCase(etsTag.displayTag);
+    taggingRecord.tag = lowerTag;
+    taggingRecord.tagWithoutHash = lowerTag.substring(1, lowerTag.length);
+    taggingRecord.save();
   }
   //let erc721Contract = HashtagProtocol.bind(event.params.nftContract);
   //tagEntity.nftContractName = erc721Contract.name();
@@ -122,7 +122,7 @@ export function handleHashtagRegistered(event: HashtagRegistered): void {
   //}
 
   // update publisher counts and fees
-  let publisherEntity = safeLoadPublisher(event.params.publisher.toHexString());
+  let publisherEntity = safeLoadPublisher(registryContract.getTaggingRecord(event.params.taggingId).value5.toString());
 
   if (publisherEntity) {
     publisherEntity.tagCount = publisherEntity.tagCount.plus(ONE);
@@ -139,7 +139,7 @@ export function handleHashtagRegistered(event: HashtagRegistered): void {
   }
 
   // update tagger counts
-  let tagger = safeLoadTagger(event.params.tagger.toHexString());
+  let tagger = safeLoadTagger(registryContract.getTaggingRecord(event.params.taggingId).value3.toString());
 
   if (tagger) {
     tagger.tagCount = tagger.tagCount.plus(ONE);
