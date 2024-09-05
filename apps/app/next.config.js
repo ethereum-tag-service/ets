a16ddf863dc75dba4f21234db621ad9049f32272
vidvidvid <weetopol@gmail.com>
2024-09-05 13:07:27 +0200

    set up million

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,6 +1,6 @@
+const MillionLint = require("@million/lint");
 const nextTranslate = require("next-translate-plugin");
 const withTM = require("next-transpile-modules")();
-
 const withWebpack = (nextConfig = {}) => {
   return {
     ...nextConfig,
@@ -10,7 +10,11 @@ const withWebpack = (nextConfig = {}) => {
         use: ["@svgr/webpack"],
       });
       // See https://www.rainbowkit.com/docs/installation#additional-build-tooling-setup
-      config.resolve.fallback = { fs: false, net: false, tls: false };
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+      };
       config.externals.push("pino-pretty", "lokijs", "encoding");
       return config;
     },
@@ -23,5 +27,4 @@ const withWebpack = (nextConfig = {}) => {
 const nextConfig = {
   reactStrictMode: true,
 };
-
-module.exports = nextTranslate(withWebpack(withTM(nextConfig)));
+module.exports = MillionLint.next()(nextTranslate(withWebpack(withTM(nextConfig))));
